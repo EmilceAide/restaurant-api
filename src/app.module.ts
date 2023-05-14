f27fda0b31d46618a0e8eda91de1fdef51f12e07
EmilceAide <emilceaide.t@gmail.com>
2023-05-14 00:06:21 -0500

    added: users, costumers, brands, categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controllers/products.controller';
 import { ProductsService } from './services/products.service';
+import { UsersController } from './controllers/users.controller';
+import { CustomersController } from './controllers/customers.controller';
+import { CategoriesController } from './controllers/categories.controller';
+import { BrandsController } from './controllers/brands.controller';
+import { UsersService } from './services/users.service';
+import { CustomersService } from './services/customers.service';
+import { CategoriesService } from './services/categories.service';
+import { BrandsService } from './services/brands.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  controllers: [
+    AppController,
+    ProductsController,
+    UsersController,
+    CustomersController,
+    CategoriesController,
+    BrandsController,
+  ],
+  providers: [
+    AppService,
+    ProductsService,
+    UsersService,
+    CustomersService,
+    CategoriesService,
+    BrandsService,
+  ],
 })
 export class AppModule {}
