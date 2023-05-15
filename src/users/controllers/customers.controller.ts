0a46060646288147fba20fe30dbf04501777a43f
EmilceAide <emilceaide.t@gmail.com>
2023-05-15 16:54:54 -0500

    users

--- src/users/controllers/customers.controller.ts
+++ src/users/controllers/customers.controller.ts
@@ -1,49 +1,42 @@
 import {
-    Controller,
-    Get,
-    Param,
-    Post,
-    Body,
-    Put,
-    Delete,
-    ParseIntPipe,
-  } from '@nestjs/common';
-  import { ApiTags, ApiOperation } from '@nestjs/swagger';
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
-  
-  import { CustomersService } from '../services/customers.service';
-  import { CreateCustomerDto, UpdateCustomerDto } from '../dtos/customer.dtos';
-  
-  @ApiTags('customers')
-  @Controller('customers')
-  export class CustomersController {
-    constructor(private customersService: CustomersService) {}
-  
-    @Get()
-    findAll() {
-      return this.customersService.findAll();
-    }
-  
-    @Get(':id')
-    get(@Param('id', ParseIntPipe) id: number) {
-      return this.customersService.findOne(id);
-    }
-  
-    @Post()
-    create(@Body() payload: CreateCustomerDto) {
-      return this.customersService.create(payload);
-    }
-  
-    @Put(':id')
-    update(
-      @Param('id', ParseIntPipe) id: number,
-      @Body() payload: UpdateCustomerDto,
-    ) {
-      return this.customersService.update(id, payload);
-    }
-  
-    @Delete(':id')
-    remove(@Param('id', ParseIntPipe) id: number) {
-      return this.customersService.remove(id);
-    }
+import { CustomersService } from '../services/customers.service';
+import { CreateCustomerDto, UpdateCustomerDto } from '../dtos/customer.dtos';
+
+@Controller('customers')
+export class CustomerController {
+  constructor(private customersService: CustomersService) {}
+
+  @Get()
+  findAll() {
+    return this.customersService.findAll();
   }
+
+  @Get(':id')
+  get(@Param('id') id: string) {
+    return this.customersService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() payload: CreateCustomerDto) {
+    return this.customersService.create(payload);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() payload: UpdateCustomerDto) {
+    return this.customersService.update(id, payload);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.customersService.remove(id);
+  }
+}
--- src/users/dtos/customer.dtos.ts
+++ src/users/dtos/customer.dtos.ts
@@ -1,21 +1,31 @@
-import { IsString, IsNotEmpty, IsPhoneNumber } from 'class-validator';
-import { PartialType, ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsArray,
+} from 'class-validator';
+import { PartialType, ApiProperty} from '@nestjs/swagger';
 
 export class CreateCustomerDto {
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
   readonly name: string;
-  
+
   @ApiProperty()
   @IsString()
   @IsNotEmpty()
   readonly lastName: string;
-  
+
   @ApiProperty()
   @IsPhoneNumber()
   @IsNotEmpty()
   readonly phone: string;
+
+  @ApiProperty()
+  @IsArray()
+  @IsNotEmpty()
+  readonly skills: any;
 }
 
 export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
