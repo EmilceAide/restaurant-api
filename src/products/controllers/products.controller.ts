20d05515e27e32524ce8878d43590bb1d283b914
EmilceAide <emilceaide.t@gmail.com>
2023-05-18 17:46:55 -0500

    products

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -9,18 +9,28 @@ import {
   Delete,
   HttpCode,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import { MongoIdPipe } from '../../common/mongo-id.pipe'
+import { MongoIdPipe } from '../../common/mongo-id.pipe';
+import { AuthGuard } from '@nestjs/passport';
 
 import { ProductsService } from '../services/products.service';
 import { CreateProductDto, UpdateProductDto, FilterProductsDto } from '../dtos/products.dtos';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles/roles.guard';
+import { Public } from '../../auth/decorators/public.decorator';
+import { Roles } from '../../auth/decorators/roles.decorator'; 
+import { Role } from '../../auth/models/roles.model'; 
 
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
+  @Public()
+  // @Roles(Role.ADMIN, Role.CUSTOMER)
   @Get()
   // @ApiOperation({ summary: 'List of products'})
   getProducts(
@@ -29,17 +39,21 @@ export class ProductsController {
     return this.productsService.findAll(params);
   }
 
+  @Public()
+  // @Roles(Role.ADMIN, Role.CUSTOMER)
   @Get(':productId')
   @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId', MongoIdPipe) productId: string) {
     return this.productsService.findOne(productId);
   }
 
+  @Roles(Role.ADMIN)
   @Post()
   create(@Body() payload: CreateProductDto) {
     return this.productsService.create(payload);
   }
 
+  @Roles(Role.ADMIN)
   @Put(':id')
   update(
     @Param('id') id: string,
@@ -48,6 +62,7 @@ export class ProductsController {
     return this.productsService.update(id, payload);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   delete(@Param('id') id: string) {
     return this.productsService.delete(id);
