1340a1b769b1791e2dc632221bde143875f938ba
EmilceAide <emilceaide.t@gmail.com>
2023-05-18 19:57:02 -0500

    port was removed

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -27,7 +27,7 @@ import config from '../config';
     {
       provide: 'MONGO',
       useFactory: async (configService: ConfigType<typeof config>) => {
-        const { connection, user, password, host, port, dbName } =
+        const { connection, user, password, host, dbName } =
           configService.mongo;
         const uri = `${connection}://${user}:${password}@${host}/?authMechanism=DEFAULT`;
         const client = new MongoClient(uri);
